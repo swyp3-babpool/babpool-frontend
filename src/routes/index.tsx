01f6fc52c084bc43c3225b4657672738226a1ffc
Mintaek_CHO <mintaek5555@gmail.com>
2024-02-17 22:08:40 +0900

    refactor: Update route paths

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,20 +3,21 @@ import {
     createRoutesFromElements,
     Route,
     RouterProvider,
-  } from "react-router-dom";
+} from 'react-router-dom';
 import HomePage from '../pages/home/HomePage';
+import SignInPage from '@/pages/signin/SignInPage';
+import SignUpPage from '@/pages/signup/SignUpPage';
 
 export default function RouteProvider() {
     const router = createBrowserRouter(
         createRoutesFromElements(
-            <Route path='/' element={<HomePage />}>
-
-            </Route>
+            <>
+                <Route path="/" element={<HomePage />} />
+                <Route path="signin" element={<SignInPage />} />
+                <Route path="signup" element={<SignUpPage />} />
+            </>
         )
-    )
-    
-    return (
-        <RouterProvider router={router} />
     );
-}
 
+    return <RouterProvider router={router} />;
+}
