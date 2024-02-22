864f86011c6c4a1b413f7589c8b9de1e3c2b96cf
chaeyoung103 <thdcodud103@naver.com>
2024-02-22 17:22:28 +0900

    feat: publishing detail page ui

--- src/pages/Notification/NotificationDetailPage.styles.tsx
+++ src/pages/Notification/NotificationDetailPage.styles.tsx
@@ -36,6 +36,44 @@ export const Header = styled.div`
     justify-content: space-between;
 `;
 
+export const ProfileInfo = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+`;
+
+export const Col = styled.div<{ gap: string }>`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: ${(props) => props.gap};
+`;
+
+export const PossibleTimeBox = styled.div`
+    width: 100%;
+    padding: 15px 16px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    border-radius: 10px;
+    border: 1px solid ${colors.purple_light_30};
+`;
+
+export const QueryBox = styled.div`
+    width: 100%;
+    height: 145px;
+    padding: 16px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    border-radius: 8px;
+    background-color: ${colors.white_10};
+`;
+
 export const BackButton = styled.button`
     width: 24px;
     height: 24px;
@@ -47,4 +85,17 @@ export const BackButton = styled.button`
 export const EmptyDiv = styled.div`
     width: 24px;
     height: 24px;
+`;
+
+export const ButtonContainer = styled.div<{ type?: string }>`
+    width: 100%;
+    padding: 0px 20px 40px;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    left: 50%;
+    bottom: 0;
+    transform: translateX(-50%);
+    gap: ${(props) => (props.type === 'received' ? '0px' : '12px')};
 `;
