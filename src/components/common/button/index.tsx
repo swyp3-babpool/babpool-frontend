230904d40ce8eec4e15496ebe64c950a260b6956
mintaek <mintaek970708@gmail.com>
2024-02-23 16:29:07 +0900

    refactor: Add type prop to Button component

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -2,34 +2,52 @@ import { colors } from '@/assets/styles/theme';
 import styled from 'styled-components';
 import Txt from '../text';
 
-type ButtonProps = {text: string, width?: string, type?: 'accept' | 'refuse', disabled?:boolean, icon?: any, onClick: () => void}
+type ButtonProps = {
+    text: string;
+    width?: string;
+    type?: 'accept' | 'refuse' | 'link';
+    disabled?: boolean;
+    icon?: any;
+    url?: string;
+    onClick?: () => void;
+};
 
-export default function Button({text, width = '100%', type = 'accept', icon, disabled=false,  onClick}: ButtonProps) {
+export default function Button({
+    text,
+    width = '100%',
+    type = 'accept',
+    icon,
+    url,
+    disabled = false,
+    onClick=()=>{},
+}: ButtonProps) {
     return (
         <ButtonWrapper width={width} type={type} disabled={disabled} onClick={onClick}>
             {icon && icon}
-            <Txt variant='h5' color={type === 'accept' ? 'white' : colors.purple_light_40}>{text}</Txt>
+            <Txt variant="h5" color={type === 'accept' ? 'white' : colors.purple_light_40}>
+                {type === 'link' ? <a href={url}>{text}</a> : text}
+            </Txt>
         </ButtonWrapper>
     );
 }
 
-export const ButtonWrapper = styled.button<{ width: string, type: string }>`
-    width: ${props => props.width};
+export const ButtonWrapper = styled.button<{ width: string; type: string }>`
+    width: ${(props) => props.width};
     min-width: 145px;
     max-width: 375px;
     height: 48px;
-    background-color: ${props => props.type === 'accept' ? colors.purple_light_40 : colors.purple_light_10};
+    background-color: ${(props) =>
+        props.type === 'accept' || props.type === 'link' ? colors.purple_light_40 : colors.purple_light_10};
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 8px;
     border-radius: 8px;
-    cursor: ${props => props.type === 'accept' ? 'pointer' : 'default'};
-`
-
-export const ButtonText = styled.p<{type: string}>`
-  font-size: 1rem;
-  font-weight: 500;
-  color: ${props => props.type === 'accept' ? colors.white : colors.purple_light_40};
+    cursor: ${(props) => (props.type === 'accept' ? 'pointer' : 'default')};
 `;
 
+export const ButtonText = styled.p<{ type: string }>`
+    font-size: 1rem;
+    font-weight: 500;
+    color: ${(props) => (props.type === 'accept' || props.type === 'link' ? colors.white : colors.purple_light_40)};
+`;
