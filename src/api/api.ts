d29c45797cc63c0138b2e408ef97514d56f9ac4d
mintaek <mintaek970708@gmail.com>
2024-03-07 15:00:56 +0900

    feat: Implement Axios interceptor

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,10 @@
 import { CommonResponseType } from "@/interface/api/commonType";
-import axios, { AxiosInstance, AxiosInterceptorManager, AxiosRequestConfig, AxiosResponse } from "axios"
+import axios, { AxiosInstance, AxiosInterceptorManager, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from "axios"
+import { error } from "console";
 
 interface CustomInstance extends AxiosInstance {
     interceptors: {
-        request: AxiosInterceptorManager<AxiosRequestConfig>;
+        request: AxiosInterceptorManager<InternalAxiosRequestConfig<any>>;
         response: AxiosInterceptorManager<AxiosResponse<CommonResponseType<any>>>;
     };
     getUri(config?: AxiosRequestConfig): string;
@@ -19,27 +20,42 @@ interface CustomInstance extends AxiosInstance {
 
 const client: CustomInstance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    withCredentials: true,
+    withCredentials: true, // cors origin 에러 방지
     timeout: 10000,
     headers: {},
 })
 
-client.interceptors.request.use(req => {
-    const token = localStorage.getItem('token')
+client.interceptors.request.use(config => {
+    const token = localStorage.getItem('accessToken')
+    config.headers['Content-Type'] = 'application/json'
+    console.log(token)
+    // 토큰이 있을 경우 헤더에 추가
     if (token) {
-        req.headers['Authorization'] =  `Bearer ${token}`
+        config.headers['Authorization'] =  `Bearer ${token}`
     }
-})
+    return config
+},
+    error => {
+        return Promise.reject(error)
+    }
+)
 
-client.interceptors.response.use(res => {
+client.interceptors.response.use(
+    res => {
     
-})
+    return res
+},
+    error => {
+        // 에러 형식에 따른 분기처리
+        return Promise.reject(error)
+    }
+)
 
-export const get = async (url: string, params: any) => {
-    const res = await client.get(url, params)
+export const get = async (url: string, params?: any) => {
+    const res: AxiosResponse<any> = await client.get(url, params)
     return res.data
 }
 export const post = async (url: string, data: any, config?: any) => {
-    const res = await client.post(url, data, config)
+    const res: AxiosResponse<any> = await client.post(url, data, config)
     return res.data
 }
