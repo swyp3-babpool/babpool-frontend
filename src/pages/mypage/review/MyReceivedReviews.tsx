238caef278bf6dfcae6416c18c7d252085610684
chaeyoung103 <thdcodud103@naver.com>
2024-03-14 12:14:16 +0900

    feat: my receivedReviews api

--- src/pages/mypage/review/MyReceivedReviews.tsx
+++ src/pages/mypage/review/MyReceivedReviews.tsx
@@ -4,43 +4,105 @@ import ReviewCount from '@/components/common/review/ReviewCount';
 import Review from '@/components/common/review/Review';
 import { Col, Row } from '@/components/common/flex/Flex';
 import { colors } from '@/assets/styles/theme';
+import {
+    ReviewCountContainer,
+    ReviewTextContainer,
+} from '@/pages/profileDetails/ProfileDetailsPage';
+import Txt from '@/components/common/text';
+import { ReviewType } from '@/interface/api/profileDetailsType';
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
+import { getReviewCounts, getReviewList } from '@/api/profile/profileApi';
+import { useLocation } from 'react-router-dom';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
+import LoadingGif from '@/assets/gif/loading.gif';
 
 export default function MyReceivedReviewsPage() {
+    const target = useRef(null);
+    const location = useLocation();
+    const profileId = location.state as string;
+    const {
+        data: reviewCounts,
+        isLoading,
+        isError,
+    } = useQuery({
+        queryKey: [`/api/review/${profileId}/count`],
+        queryFn: () => getReviewCounts(profileId as string),
+    });
+
+    console.log(reviewCounts);
+
+    const { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
+        queryKey: ['/api/review/list', profileId],
+        queryFn: ({ pageParam = 0 }) =>
+            getReviewList({
+                profileId: profileId,
+                page: pageParam,
+                size: 10,
+            }),
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, allPages) => {
+            const nextPage =
+                allPages.length !== allPages[0].totalPages
+                    ? lastPage.pageable.pageNumber + 1
+                    : undefined;
+            return nextPage;
+        },
+        getPreviousPageParam: (firstPage, allPages) => {
+            return allPages.length !== allPages[0].totalPages
+                ? firstPage.pageable.pageNumber - 1
+                : undefined;
+        },
+    });
+
+    const { observe, unobserve } = useIntersectionObserver(fetchNextPage);
+
+    useEffect(() => {
+        if (target.current) {
+            observe(target.current);
+        }
+        return () => {
+            if (target.current) {
+                unobserve(target.current);
+            }
+        };
+    }, [data]);
+
     return (
-        <ReceivedReviewPageContainer>
-            <Header text={`내가 받은 후기`} />
-            <ReviewContainer>
-                <Row gap={12} padding="0 10px">
-                    {['최고예요', '좋아요', '별로예요'].map((text) => (
-                        <ReviewCount key={text} text={text} count={1} />
-                    ))}
-                </Row>
-                <ScrollableCol gap={16}>
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                    <Review text="최고의 컨설팅을 해주셔서 감사합니다" />
-                </ScrollableCol>
-            </ReviewContainer>
-        </ReceivedReviewPageContainer>
+        reviewCounts &&
+        isSuccess &&
+        !isLoading && (
+            <ReceivedReviewPageContainer>
+                <Header text={`내가 받은 후기`} />
+                <ReviewContainer>
+                    <ReviewCountContainer>
+                        <ReviewCount count={reviewCounts.bestCount} text="최고예요" />
+                        <ReviewCount count={reviewCounts.greatCount} text="좋아요" />
+                        <ReviewCount count={reviewCounts.badCount} text="별로에요" />
+                    </ReviewCountContainer>
+                    <ReviewTextContainer>
+                        {data.pages[0].content.length !== 0 ? (
+                            <>
+                                {data.pages.map((page) => {
+                                    return page.content.map((review: ReviewType) => {
+                                        return <Review text={review.reviewComment} />;
+                                    });
+                                })}
+                                {hasNextPage && (
+                                    <ObserverTarget ref={target}>
+                                        <Loading src={LoadingGif} alt="로딩" />
+                                    </ObserverTarget>
+                                )}
+                            </>
+                        ) : (
+                            <Txt variant="caption1" color={colors.white_20}>
+                                아직 리뷰가 없습니다.
+                            </Txt>
+                        )}
+                    </ReviewTextContainer>
+                </ReviewContainer>
+            </ReceivedReviewPageContainer>
+        )
     );
 }
 
@@ -54,6 +116,13 @@ const ReceivedReviewPageContainer = styled.div`
     align-items: center;
     justify-content: flex-start;
     overflow: hidden;
+`;
+
+const ObserverTarget = styled.div`
+    width: 100%;
+    height: 30px;
+    display: grid;
+    place-items: center;
 `;
 
 const ScrollableCol = styled(Col)`
@@ -74,4 +143,9 @@ const ReviewContainer = styled.div`
     flex-direction: column;
     margin-top: 25px;
     gap: 30px;
+`;
+
+const Loading = styled.img`
+    width: 25px;
+    height: 25px;
 `;
