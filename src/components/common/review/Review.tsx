52d8c4128e2cc33a79f62aafef9da6979936549d
chaeyoung103 <thdcodud103@naver.com>
2024-03-19 21:48:39 +0900

    fix: fix flexible review box

--- src/components/common/review/Review.tsx
+++ src/components/common/review/Review.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { styled } from 'styled-components';
 import Txt from '../text';
-import {ReactComponent as UserIcon} from '@/assets/icons/ic_user.svg';
+import { ReactComponent as UserIcon } from '@/assets/icons/ic_user.svg';
 import { colors } from '@/assets/styles/theme';
 
-export default function Review({text}: {text: string}) {
+export default function Review({ text }: { text: string }) {
     return (
         <ReviewBox>
-                <UserIcon />
+            <UserIcon />
             <ReviewTextBox>
-                <Txt variant='caption1'>{text}</Txt>
+                <Txt variant="caption1">{text}</Txt>
             </ReviewTextBox>
         </ReviewBox>
     );
@@ -20,14 +20,15 @@ const ReviewBox = styled.div`
     display: flex;
     align-items: center;
     gap: 9px;
-`
+`;
 
 const ReviewTextBox = styled.div`
     width: 100%;
-    height: 42px;
+    height: auto;
     padding: 10px;
     display: flex;
     align-items: center;
+    justify-content: flex-start;
     background-color: ${colors.white_10};
     border-radius: 8px;
-`
+`;
