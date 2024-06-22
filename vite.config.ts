104dd77debaa5ca0541beaa2eca0b911a6c2c86b
goops2000 <goops2000@naver.com>
2024-06-22 19:23:35 +0900

    feat: vite remove console 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,27 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from '@svgr/rollup';
 import path from 'path';
+import removeConsole from 'vite-plugin-remove-console';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), svgr()],
-    resolve: {
-        alias: [
-            {
-                find: '@',
-                replacement: path.resolve(__dirname, 'src'),
+export default defineConfig(({ command }) => {
+    return {
+        plugins: [react(), svgr(), command === 'build' && removeConsole()],
+        resolve: {
+            alias: [
+                {
+                    find: '@',
+                    replacement: path.resolve(__dirname, 'src'),
+                },
+            ],
+        },
+        server: {
+            proxy: {
+                '/websocket': {
+                    target: 'https://api.babpool.com',
+                    ws: true,
+                },
             },
-        ],
-    },
-    server: {
-        proxy: {
-            '/websocket': {
-                target: 'https://api.babpool.com',
-                ws: true,
-            }
-        }
-    }
-
+        },
+    };
 });
