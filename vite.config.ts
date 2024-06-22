bb5adb7c521e8f09514f61021b92ad0fe15e1880
goops2000 <goops2000@naver.com>
2024-06-22 22:48:59 +0900

    fix: plugin type 에러 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,55 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from '@svgr/rollup';
 import path from 'path';
 import removeConsole from 'vite-plugin-remove-console';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-    return {
-        plugins: [react(), svgr(), command === 'build' && removeConsole()],
-        resolve: {
-            alias: [
-                {
-                    find: '@',
-                    replacement: path.resolve(__dirname, 'src'),
-                },
-            ],
-        },
-        server: {
-            proxy: {
-                '/websocket': {
-                    target: 'https://api.babpool.com',
-                    ws: true,
-                },
+// 개발용 설정
+const devConfig = {
+    plugins: [react(), svgr()] as PluginOption[],
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: path.resolve(__dirname, 'src'),
+            },
+        ],
+    },
+    server: {
+        proxy: {
+            '/websocket': {
+                target: 'https://api.babpool.com',
+                ws: true,
             },
         },
-    };
+    },
+};
+
+// 빌드용 설정
+const buildConfig = {
+    plugins: [react(), svgr(), removeConsole()] as PluginOption[],
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: path.resolve(__dirname, 'src'),
+            },
+        ],
+    },
+    server: {
+        proxy: {
+            '/websocket': {
+                target: 'https://api.babpool.com',
+                ws: true,
+            },
+        },
+    },
+};
+
+export default defineConfig(({ command }) => {
+    if (command === 'build') {
+        return buildConfig;
+    } else {
+        return devConfig;
+    }
 });
